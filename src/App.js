80bac7d33ffe398a996ffa1b4e7cca8fe3948970
Joanna <jiaoni.wang.01@gmail.com>
2022-06-10 08:26:18 -0400

    switched to hashrouter

--- src/App.js
+++ src/App.js
@@ -3,20 +3,18 @@ import Landing from './Landing';
 import Services from './Services';
 import Staff from './Staff';
 import {
-  HashRouter,
   Routes,
   Route
 } from "react-router-dom";
 
 function App() {
-  return(<HashRouter>
+  return(
       <Routes>
         <Route path='/' element={<Landing />} /> 
         <Route path='/service' element={<Services />} />
         <Route path='/book' element={<BookAppt />} />
         <Route path='/staff' element={<Staff />} />
-      </Routes>
-    </HashRouter>);
+      </Routes>);
 }
 
 export default App;
